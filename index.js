aeb619b5fb565ec8d456daffd12bf53d1d2dd81e
Cem <cemkgrbz@gmail.com>
2022-07-18 16:05:49 +0200

    modified index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 // 1
 // Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
 function vowel_count(str1) {
-  //write your code here
+  const vowelList = 'aeiouAEIOU';
+  let vowelCount = 0; 
+  for (let i = 0; i < str1.length; i++) {
+    if (vowelList.indexOf(str1[i]) !== -1) {
+      vowelCount += 1;
+    }
+  }
+  return vowelCount
+
 }
 console.log(vowel_count("The quick brown fox"));
 
 //2
 // Write a JavaScript function that generates a string id (specified length) of random characters.
 function makeid(l) {
-  //write your code here
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let gen = "";
+  for (let i = 0; i < l; i++) {
+    gen += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return gen
 }
 console.log(makeid(8));
 
